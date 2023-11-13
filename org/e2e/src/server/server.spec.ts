fc543607ce64a9d2edb5a117cb195d9d32303235
Panagiotis Doxopoulos <P28375@eon.com>
2023-11-13 18:01:05 +0100

    fix: moved api tests to e2e folder

--- org/e2e/src/server/server.spec.ts
+++ org/e2e/src/server/server.spec.ts
@@ -8,3 +8,31 @@ describe('GET /', () => {
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
+
+describe('GET /accounts', () => {
+  it('should return a message', async () => {
+    const res = await axios.get('/accounts');
+
+    expect(res.status).toBe(201);
+  });
+});
+
+describe('POST /accounts', () => {
+  it('should return a message', async () => {
+    const data = JSON.stringify({ name: 'Babis' });
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/accounts',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data,
+    };
+
+    const response = await axios.request(config);
+
+    expect(response.status).toBe(200);
+  });
+});
